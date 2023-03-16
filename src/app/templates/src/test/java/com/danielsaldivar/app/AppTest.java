3775ab85aa35d01fefb325ee7e199e545801861c
Daniel Saldivar <daniel.saldivar.ds@gmail.com>
2023-03-15 22:14:31 -0400

    fix: made it so tests actually pass

--- src/app/templates/src/test/java/com/danielsaldivar/app/AppTest.java
+++ src/app/templates/src/test/java/com/danielsaldivar/app/AppTest.java
@@ -1,14 +1,14 @@
 package com.danielsaldivar.app;
 
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
 public class AppTest 
 {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+      assertThat(true)
+        .isTrue();
     }
 }
